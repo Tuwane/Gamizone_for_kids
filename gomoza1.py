0deb73065bca91a942169a6fc7c0bc97486282b0
Tuwane <152686997+Tuwane@users.noreply.github.com>
2026-09-18 21:13:52 +0200

    Need to be updated

---
+++ gomoza1.py
@@ -0,0 +1,8 @@
+num1 = input("Enter first number: ")
+num2 = input("Enter second number: ")
+
+# Convert the inputs from text (strings) to decimal numbers (floats)
+sum_result = float(num1) + float(num2)
+
+# Print the final answer
+print(f"The sum of {num1} and {num2} is {sum_result}")
